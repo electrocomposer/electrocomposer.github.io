e37fb34a08f732695e67d3ef75156f1035a970d9
mattheweq <repos@mattheweq.com>
2025-04-15 20:13:00 +0100

    instantiate v1

---
+++ app.js
@@ -0,0 +1,12 @@
+const loader = document.getElementById('loader');
+const content = document.getElementById('content');
+
+content.style.display = "none";
+
+setTimeout(() => {
+  loader.style.display = "none";
+
+  window.location = "/api/";
+  document.body.style.backgroundColor = "white";
+  content.style.display = "block";
+}, 2000);
